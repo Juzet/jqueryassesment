dfecbaf9b7e3c975dbe0a8a1ff77daac6aecfd1c
Juzet Arana <arana.juzet@gmail.com>
2015-04-20 12:05:56 -0700

    to see if it works

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $( ".dan" ).click(function() {
+//     console.log( "hello" );
+// });
+// $( "#iwao" ).on( "click", function() {
+//     alert( "click" );
+// });
+$(document).ready(function(){
+    $( "#iwao" ).on( "click", function() {
+        alert( "fkqojnx" );
+    });
+$(document).ready(function(){
+    $( ".dan" ).on( "click", function() {
+        alert( "fkqojnx" );
+    });
